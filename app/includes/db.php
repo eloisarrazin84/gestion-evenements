b465cdc2b97c9fe9a101ade1957c03ae79c91120
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-12-07 10:45:36 +0100

    Update db.php

--- app/includes/db.php
+++ app/includes/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'gestion_evenements';
 $username = 'root';
-$password = '';
+$password = 'rootpassword';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
